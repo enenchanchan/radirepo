e2552d7978943ac02b6e234334ed0c4196a4b774
enenchanchan <20enchange22@gmail.com>
2022-04-29 15:19:46 +0900

    nav修正

--- src/resources/views/articles/toppage.blade.php
+++ src/resources/views/articles/toppage.blade.php
@@ -1,6 +1,7 @@
 @extends('app')
 @section('title','記事一覧')
 @section('content')
+@extends('nav')
 <div class="container">
     <h1>hello</h1>
 </div>
---
+++ src/resources/views/nav.blade.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: darkorange;">
+    <div class="container">
+        <a href="" class="navbar-brand">radirepo <i class="fa-solid fa-pen"></i></a>
+        <ul class="navbar-nav">
+            <li class="nav-item"> <a href="" class="nav-link"> ユーザー登録</a></li>
+            <li class="nav-item"><a href="" class="nav-link"> ログイン</a></li>
+            <li class="nav-item"><a href="" class="nav-link"> 投稿する</a></li>
+        </ul>
+    </div>
+
+</nav>
