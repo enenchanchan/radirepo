bc434320446c6489623c1d62ef00f2d1394e696b
enenchanchan <20enchange22@gmail.com>
2022-05-05 14:03:05 +0900

    一覧ページ作成

--- src/app/Http/Controllers/ArticleController.php
+++ src/app/Http/Controllers/ArticleController.php
@@ -2,47 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        $articles = [
-            (object)[
-                'id' => 1,
-                'radio_title' => '番組名1',
-                'radio_date' => "放送日時1",
-                'body' => '本文1',
-                'created_at' => now(),
-                'user' => (object)[
-                    'id' => 1,
-                    'name' => 'ユーザー名1',
-                ],
-            ],
-            (object)[
-                'id' => 1,
-                'radio_title' => '番組名2',
-                'radio_date' => "放送日時2",
-                'body' => '本文1',
-                'created_at' => now(),
-                'user' => (object)[
-                    'id' => 1,
-                    'name' => 'ユーザー名2',
-                ],
-            ],
-            (object)[
-                'id' => 1,
-                'radio_title' => '番組名3',
-                'radio_date' => '放送日時3',
-                'body' => '本文1',
-                'created_at' => now(),
-                'user' => (object)[
-                    'id' => 1,
-                    'name' => 'ユーザー名3',
-                ],
-            ],
-        ];
+        $articles = Article::all()->sortByDesc('created_at');
         return view('articles.index', ['articles' => $articles]);
     }
 }
