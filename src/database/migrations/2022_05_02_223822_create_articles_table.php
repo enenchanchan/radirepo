fb3dd3b5ed0af4e7788e2f43bee450ed564e40c2
enenchanchan <20enchange22@gmail.com>
2022-05-05 13:48:44 +0900

    マイグレーション

--- src/database/migrations/2022_05_02_223822_create_articles_table.php
+++ src/database/migrations/2022_05_02_223822_create_articles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateArticlesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,11 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::create('articles', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->unsignedBigInteger('id');
             $table->string('radio_title');
             $table->date('radio_date');
             $table->text('body');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
         });
     }
