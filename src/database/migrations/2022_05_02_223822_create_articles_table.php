449e821db90d1bba0ef4693be57862635e3f63b5
enenchanchan <20enchange22@gmail.com>
2022-05-05 21:57:25 +0900

    記事投稿完成

--- src/database/migrations/2022_05_02_223822_create_articles_table.php
+++ src/database/migrations/2022_05_02_223822_create_articles_table.php
@@ -14,7 +14,7 @@ class CreateArticlesTable extends Migration
     public function up()
     {
         Schema::create('articles', function (Blueprint $table) {
-            $table->unsignedBigInteger('id');
+            $table->bigIncrements('id');
             $table->string('radio_title');
             $table->date('radio_date');
             $table->text('body');
